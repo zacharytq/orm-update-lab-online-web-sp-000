f92189a662c6cf2ac5064479b4f00d5d443e769f
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 01:44:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,4 +57,17 @@ class Student
     student.save
     student
   end
+
+  def self.new_from_db(row)
+    student = Student.new(row[1],row[2], row[0])
+    student
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE students.name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map {}
+  end
 end
