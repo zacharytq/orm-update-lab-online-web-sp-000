820f45532c2bd9ae00efcd27713c5fb234bf565a
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 01:19:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,12 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
 end
